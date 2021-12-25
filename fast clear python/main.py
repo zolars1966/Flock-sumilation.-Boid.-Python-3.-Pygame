7f97f68f5db371f65e2975c27bdb682a32091c4e
Arseny Zolotarev <70763346+ckreesher@users.noreply.github.com>
2021-12-26 01:18:47 +0800

    Fast code & more perfect vertical synchronization

---
+++ fast clear python/main.py
@@ -0,0 +1,58 @@
+from random import *
+from flock import Flock
+import pygame as pg
+import math
+import sys
+
+radian = math.pi/180
+TICK_RATE = 128
+size = width, height = 1280, 720
+
+
+def length2D(vec):
+    return math.sqrt(vec[0]*vec[0] + vec[1]*vec[1])
+
+
+def draw_boids(sc, boids):
+    for boid in boids:
+        pg.draw.circle(sc, "black", boid.pos, 5)
+        lv = length2D(boid.velocity)
+        xpos = boid.pos[0] + 10*boid.velocity[0]/lv, boid.pos[1] + 10*boid.velocity[1]/lv
+        pg.draw.line(sc, "black", boid.pos, xpos, 2)
+
+
+if __name__ == "__main__":
+    screen = pg.display.set_mode(size, vsync=1)
+    clock = pg.time.Clock()
+
+    flock = Flock(30)
+
+    upd_ticks = pg.time.get_ticks()
+
+    while True:
+        screen.fill("white")
+
+        keys = pg.key.get_pressed()
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                sys.exit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_u:
+                    TICK_RATE -= 1
+                    upd_ticks = pg.time.get_ticks()
+                if event.key == pg.K_i:
+                    TICK_RATE += 1
+                    upd_ticks = pg.time.get_ticks()
+
+        draw_boids(screen, flock.boids)
+
+        ticks = pg.time.get_ticks()
+        if ticks - upd_ticks >= (1000 / TICK_RATE):
+            for i in range(int((ticks - upd_ticks) / (1000 / TICK_RATE))):
+                flock.run()
+            upd_ticks = ticks - (ticks - upd_ticks) % (1000 / TICK_RATE)
+
+        pg.display.set_caption("$~Boids ~fps: " + str(round(clock.get_fps(), 2)) + " ~tickrate: " + str(TICK_RATE))
+
+        pg.display.flip()
+        clock.tick(60)
